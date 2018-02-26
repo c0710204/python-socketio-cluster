b004afd4e52f1686ed1fe5472412af4137047f20
Xiang Gu <c0710204@gmail.com>
2018-02-25 19:09:00 -0800

    fix arg

--- server/sdn.py
+++ server/sdn.py
@@ -73,7 +73,7 @@ if __name__ == "__main__":
         default="config/sdn.yaml",
         help='path of config file')
 
-    args = parser.parse_args()
+    args = parser.parse_args() 
 
     clients = {}
     procs = []
