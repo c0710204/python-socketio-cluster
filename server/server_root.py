016e8b6ceffc9578b18bf225004a0e42a0ab3e88
Xiang Gu <c0710204@gmail.com>
2018-02-27 15:56:06 -0800

    add terminate

--- server/server_root.py
+++ server/server_root.py
@@ -3,9 +3,15 @@ import subprocess
 def worker(i):
     subprocess.Popen("python {0}".format(i),shell=True)
 def main():
-    tasks=["server/sdn.py","pspnet/pspnet_serving_test.py","server/logserv.py"]
-    for i in tasks:
-        p = multiprocessing.Process(target=worker(i))
-        p.start()
+    p_list=[]
+    try:
+        tasks=["server/sdn.py","pspnet/pspnet_serving_test.py","server/logserv.py"]
+        for i in tasks:
+            p = multiprocessing.Process(target=worker(i))
+            p.start()
+        p_list.push(p)
+    except Exception as e:
+        for p in p_list:
+            p.terminate()
 if __name__ == '__main__':
     main()
