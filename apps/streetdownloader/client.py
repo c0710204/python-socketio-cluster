22fe88b758c0102031e436bd6c4fc1ec8506a031
Xiang Gu <c0710204@gmail.com>
2018-03-01 16:12:02 -0800

    ddd

--- apps/streetdownloader/client.py
+++ apps/streetdownloader/client.py
@@ -1,14 +1,30 @@
 import apps.streetdownloader.pkg.streetview as streetview
+import logging
+import subprocess
 class app_client():
+    def scp_download(self,port, user, host, path):
+        cmd = "scp -P {0} {1}@{2}:{3} ./".format(port, user, host, path)
+        logging.info(cmd)
+        ret = subprocess.call(cmd, shell=True)
+    def scp_upload(self,port, user, host, path, file):
+        cmd = "scp -P {0} ./{4} {1}@{2}:{3} ".format(port, user, host, path, file)
+        logging.info(cmd)
+        ret = subprocess.call(cmd, shell=True)
+    def sshdownload(self,data):
+        print("downloading {0}...".format(data['input_path']))
+        scp_download(data['ssh']['port'], data['ssh']['username'], "127.0.0.1",
+                     data['input_path'])
+    def sshupload(self,data, path):
+        print("uploading {0}...".format(data['input_path']))
+        scp_upload(data['ssh']['port'], data['ssh']['username'], "127.0.0.1",
+                   data["output_path"], path)
     def __init__(self):
-            pass
+        pass
 class stv_app_client(app_client):
     def run(self,args):
         """
         :param args all needed data from server
         """
-
-
         panoids = streetview.panoids(lat= args['lat'], lon= args['lon'])
         return panoids
 
---
+++ apps/streetdownloader/db.py
@@ -0,0 +1,3 @@
+import sqlite3
+database="db.sqlite3"
+conn=sqlite3.connect(database)
