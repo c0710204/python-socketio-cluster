6499d9941cc741013e2e154a95225026d4456eae
Xiang Gu <c0710204@gmail.com>
2018-03-02 17:03:57 -0800

    imp

--- apps/streetdownloader/client.py
+++ apps/streetdownloader/client.py
@@ -3,7 +3,7 @@ import logging
 import subprocess
 
 import multiprocessing
-
+import sys
 from socketIO_client import SocketIO, LoggingNamespace, BaseNamespace
 class app_client(BaseNamespace):
     def on_ask_init(self,*args):
