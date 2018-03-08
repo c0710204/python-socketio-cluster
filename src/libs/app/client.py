4daed8090277579a7c2693b2d8e6863a36f9af5a
Xiang Gu <c0710204@gmail.com>
2018-03-07 18:24:58 -0800

    dbg

--- src/libs/app/client.py
+++ src/libs/app/client.py
@@ -5,6 +5,9 @@ import sys
 
 from socketIO_client import SocketIO, LoggingNamespace, BaseNamespace
 class app_client(BaseNamespace):
+    def __init__(self,*args):
+        BaseNamespace.__init__(self,*args):
+        self.prepare()
     def prepare(self):
         """
         run on the start and init all
