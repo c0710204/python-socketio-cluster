3523ddcc3df6c93e63c737aa0d588eec100d6bb1
Xiang Gu <c0710204@gmail.com>
2018-03-07 16:30:11 -0800

    s

--- src/client/client.py
+++ src/client/client.py
@@ -9,7 +9,7 @@ def main():
     parser = argparse.ArgumentParser(description='distrube client')
     parser.add_argument('--client','-c', type=str,help='an integer for the accumulator')
     args=parser.parse_args()
-    cli=importlib.import_module("app.{0}.client".format(args.client))
+    cli=importlib.import_module("apps.{0}.client".format(args.client))
     cli_handle=cli.handler()
     asio = async_socketIO(SocketIO('localhost', 30021))
     sio_pspent_info = asio.socketIO.define(cli_handle, '/task')
