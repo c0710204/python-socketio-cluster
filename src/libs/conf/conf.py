6b2f4765d89caaa8ffdeae5a404a95ccf65bae7e
Xiang Gu <c0710204@gmail.com>
2018-04-09 16:59:40 -0700

    dbg

--- src/libs/conf/conf.py
+++ src/libs/conf/conf.py
@@ -5,7 +5,7 @@ class conf(object):
     def __init__(self):
         #read config @target
         self._configs=dict()
-    def load(target,path=None):
+    def load(self,target,path=None):
         if path==None:
             path="config/{0}.yaml".format(target)
         if os.path.isfile(path):
