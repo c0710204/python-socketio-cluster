ec284f23c31a4906e55cbc9b689977b08989c956
Xiang Gu <c0710204@gmail.com>
2018-04-12 12:53:24 -0700

    dbg

--- apps/pspnet/server.py
+++ apps/pspnet/server.py
@@ -41,7 +41,7 @@ class pspnet_app_server(app_server):
         rnd=os.getpid()
 
         sql1='update tasks set status="loaded" , locker="{0}" where status="wait" limit 1'.format(rnd)
-        sql2='select tasks.id as id pid,path,resultpath from files,tasks where tasks.loader="{0}" tasks.pid=files.pid  and tasks.status="loaded" limit 1'.format(rnd)
+        sql2='select tasks.id as id pid,path,resultpath from files,tasks where tasks.loader="{0}" and tasks.pid=files.pid  and tasks.status="loaded" limit 1'.format(rnd)
         cursor.execute(sql1);
         cursor.execute(sql2);
         self.db.commit()
