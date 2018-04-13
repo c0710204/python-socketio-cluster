15852b6179b631945dbd9c099a288ce1971a47a2
Xiang Gu <c0710204@gmail.com>
2018-04-12 20:17:23 -0700

    add full mysql support

--- server/logserv.py
+++ server/logserv.py
@@ -41,25 +41,32 @@ def update(sid, data):
         cur=mysqldb.cursor()
         cur.execute(sql2)
         mysqldb.commit()
+        sql="select * from psplog where time in (SELECT max(time) FROM gsv_file_list.psplog group by panid) order by time"
 
-    if data['id'] in db:
-        if (data['max']):
-            db[data['id']]['max'] = data['max']
-
-        if (data['val']):
-            db[data['id']]['val'] = data['val']
-
-        if (data['phase']):
-            db[data['id']]['phase'] = data['phase']
+        cur=mysqldb.cursor()
+        cur.execute(sql2)
+        mysqldb.commit()
+        lines=cur.fetchall()
+        sio.emit('progress_upgrade_server',lines)
     else:
-        data['label']=data['id']
-        db[data['id']]=data
-    try:
-        collection.insert_one(data)
-    except Exception as e:
+        if data['id'] in db:
+            if (data['max']):
+                db[data['id']]['max'] = data['max']
+
+            if (data['val']):
+                db[data['id']]['val'] = data['val']
+
+            if (data['phase']):
+                db[data['id']]['phase'] = data['phase']
+        else:
+            data['label']=data['id']
+            db[data['id']]=data
+        try:
+            collection.insert_one(data)
+        except Exception as e:
+            pass
+        sio.emit('progress_upgrade_server',data=db.values())
         pass
-    sio.emit('progress_upgrade_server',data=db.values())
-    pass
 
 @sio.on('disconnect')
 def disconnect(sid):
